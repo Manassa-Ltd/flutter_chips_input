aa2f3c13d7a07582c79fddf9fd404a4eb6d6a73d
Manassa LTD. <info@manassa.ly>
2025-11-20 14:00:23 +0200

    chore: upgrade example sdk to >=3.0.0, update builder signatures and add onSubmit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chips_input/flutter_chips_input.dart';
 
+const String avatarPlaceholder = "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -24,39 +26,27 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   final _chipKey = GlobalKey<ChipsInputState>();
 
   @override
   Widget build(BuildContext context) {
     const mockResults = <AppProfile>[
-      AppProfile('John Doe', 'jdoe@flutter.io',
-          'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4057996.jpg'),
-      AppProfile('Paul', 'paul@google.com',
-          'https://mbtskoudsalg.com/images/person-stock-image-png.png'),
-      AppProfile('Fred', 'fred@google.com',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('Brian', 'brian@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('John', 'john@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('Thomas', 'thomas@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('Nelly', 'nelly@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('Marie', 'marie@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('Charlie', 'charlie@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('Diana', 'diana@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('Ernie', 'ernie@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
-      AppProfile('Gina', 'fred@flutter.io',
-          'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'),
+      AppProfile('John Doe', 'jdoe@flutter.io', 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4057996.jpg'),
+      AppProfile('Paul', 'paul@google.com', avatarPlaceholder),
+      AppProfile('Fred', 'fred@google.com', avatarPlaceholder),
+      AppProfile('Brian', 'brian@flutter.io', avatarPlaceholder),
+      AppProfile('John', 'john@flutter.io', avatarPlaceholder),
+      AppProfile('Thomas', 'thomas@flutter.io', avatarPlaceholder),
+      AppProfile('Nelly', 'nelly@flutter.io', avatarPlaceholder),
+      AppProfile('Marie', 'marie@flutter.io', avatarPlaceholder),
+      AppProfile('Charlie', 'charlie@flutter.io', avatarPlaceholder),
+      AppProfile('Diana', 'diana@flutter.io', avatarPlaceholder),
+      AppProfile('Ernie', 'ernie@flutter.io', avatarPlaceholder),
+      AppProfile('Gina', 'fred@flutter.io', avatarPlaceholder),
     ];
 
     return Scaffold(
@@ -67,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              ChipsInput(
+              ChipsInput<AppProfile>(
                 key: _chipKey,
                 /*initialValue: [
                   AppProfile('John Doe', 'jdoe@flutter.io',
@@ -79,32 +69,23 @@ class _MyHomePageState extends State<MyHomePage> {
                 textCapitalization: TextCapitalization.words,
                 // enabled: false,
                 // maxChips: 5,
-                textStyle: const TextStyle(
-                    height: 1.5, fontFamily: 'Roboto', fontSize: 16),
+                textStyle: const TextStyle(height: 1.5, fontFamily: 'Roboto', fontSize: 16),
                 decoration: const InputDecoration(
-                  // prefixIcon: Icon(Icons.search),
+                  prefixIcon: Icon(Icons.search),
                   // hintText: formControl.hint,
                   labelText: 'Select People',
-                  // enabled: false,
+                  enabled: false,
                   // errorText: field.errorText,
+                  border: OutlineInputBorder(),
                 ),
-                findSuggestions: (String query) {
+                findSuggestions: (query) {
                   // print("Query: '$query'");
                   if (query.isNotEmpty) {
                     var lowercaseQuery = query.toLowerCase();
                     return mockResults.where((profile) {
-                      return profile.name
-                              .toLowerCase()
-                              .contains(query.toLowerCase()) ||
-                          profile.email
-                              .toLowerCase()
-                              .contains(query.toLowerCase());
+                      return profile.name.toLowerCase().contains(query.toLowerCase()) || profile.email.toLowerCase().contains(query.toLowerCase());
                     }).toList(growable: false)
-                      ..sort((a, b) => a.name
-                          .toLowerCase()
-                          .indexOf(lowercaseQuery)
-                          .compareTo(
-                              b.name.toLowerCase().indexOf(lowercaseQuery)));
+                      ..sort((a, b) => a.name.toLowerCase().indexOf(lowercaseQuery).compareTo(b.name.toLowerCase().indexOf(lowercaseQuery)));
                   }
                   // return <AppProfile>[];
                   return mockResults;
@@ -112,39 +93,41 @@ class _MyHomePageState extends State<MyHomePage> {
                 onChanged: (data) {
                   // print(data);
                 },
-                chipBuilder: (context, state, dynamic profile) {
+                chipBuilder: (context, state, index, profile) {
                   return InputChip(
                     key: ObjectKey(profile),
                     label: Text(profile.name),
-                    avatar: CircleAvatar(
-                      backgroundImage: NetworkImage(profile.imageUrl),
-                    ),
-                    onDeleted: () => state.deleteChip(profile),
+                    avatar: CircleAvatar(backgroundImage: NetworkImage(profile.imageUrl)),
+                    onDeleted: () => state.deleteChip(index),
                     materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                   );
                 },
-                suggestionBuilder: (context, state, dynamic profile) {
-                  return ListTile(
+                suggestionBuilder: (context, state, index, profile) {
+                  return InputChip(
                     key: ObjectKey(profile),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(profile.imageUrl),
-                    ),
-                    title: Text(profile.name),
-                    subtitle: Text(profile.email),
-                    onTap: () => state.selectSuggestion(profile),
+                    label: Text(profile.name),
+                    avatar: CircleAvatar(backgroundImage: NetworkImage(profile.imageUrl)),
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    onPressed: () => state.selectSuggestion(profile),
+                  );
+                },
+                onSubmit: (txt) {
+                  if (txt.isEmpty) return null;
+                  return mockResults.firstWhere(
+                        (profile) => profile.name == txt,
+                    orElse: () => AppProfile(txt, "", "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"),
                   );
                 },
               ),
               const TextField(),
-              /*ChipsInput(
-                initialValue: [
-                  AppProfile('John Doe', 'jdoe@flutter.io',
-                      'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4057996.jpg'),
+              ChipsInput(
+                initialValue: const [
+                  AppProfile('John Doe', 'jdoe@flutter.io', 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX4057996.jpg'),
                 ],
                 enabled: true,
                 maxChips: 10,
-                textStyle: TextStyle(height: 1.5, fontFamily: "Roboto", fontSize: 16),
-                decoration: InputDecoration(
+                textStyle: const TextStyle(height: 1.5, fontFamily: "Roboto", fontSize: 16),
+                decoration: const InputDecoration(
                   // prefixIcon: Icon(Icons.search),
                   // hintText: formControl.hint,
                   labelText: "Select People",
@@ -152,21 +135,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   // errorText: field.errorText,
                 ),
                 findSuggestions: (String query) {
-                  if (query.length != 0) {
+                  if (query.isNotEmpty) {
                     var lowercaseQuery = query.toLowerCase();
                     return mockResults.where((profile) {
-                      return profile.name
-                          .toLowerCase()
-                          .contains(query.toLowerCase()) ||
-                          profile.email
-                              .toLowerCase()
-                              .contains(query.toLowerCase());
+                      return profile.name.toLowerCase().contains(query.toLowerCase()) || profile.email.toLowerCase().contains(query.toLowerCase());
                     }).toList(growable: false)
-                      ..sort((a, b) => a.name
-                          .toLowerCase()
-                          .indexOf(lowercaseQuery)
-                          .compareTo(
-                          b.name.toLowerCase().indexOf(lowercaseQuery)));
+                      ..sort((a, b) => a.name.toLowerCase().indexOf(lowercaseQuery).compareTo(b.name.toLowerCase().indexOf(lowercaseQuery)));
                   } else {
                     return mockResults;
                   }
@@ -174,35 +148,35 @@ class _MyHomePageState extends State<MyHomePage> {
                 onChanged: (data) {
                   print(data);
                 },
-                chipBuilder: (context, state, profile) {
+                chipBuilder: (context, state, index, profile) {
                   return InputChip(
                     key: ObjectKey(profile),
                     label: Text(profile.name),
-                    avatar: CircleAvatar(
-                      backgroundImage: NetworkImage(profile.imageUrl),
-                    ),
-                    onDeleted: () => state.deleteChip(profile),
+                    avatar: CircleAvatar(backgroundImage: NetworkImage(profile.imageUrl)),
+                    onDeleted: () => state.deleteChip(index),
                     materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                   );
                 },
-                suggestionBuilder: (context, state, profile) {
+                suggestionBuilder: (context, state, index, profile) {
                   return ListTile(
                     key: ObjectKey(profile),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(profile.imageUrl),
-                    ),
+                    leading: CircleAvatar(backgroundImage: NetworkImage(profile.imageUrl)),
                     title: Text(profile.name),
                     subtitle: Text(profile.email),
                     onTap: () => state.selectSuggestion(profile),
                   );
                 },
-              ),*/
+                onSubmit: (txt) {
+                  if (txt.isEmpty) return null;
+                  return mockResults.firstWhere(
+                        (profile) => profile.name == txt,
+                    orElse: () => AppProfile(txt, "", avatarPlaceholder),
+                  );
+                },
+              ),
               ElevatedButton(
                 onPressed: () {
-                  _chipKey.currentState!.selectSuggestion(const AppProfile(
-                      'Gina',
-                      'fred@flutter.io',
-                      'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'));
+                  _chipKey.currentState!.selectSuggestion(const AppProfile('Gina', 'fred@flutter.io', avatarPlaceholder));
                 },
                 child: const Text('Add Chip'),
               ),
@@ -222,11 +196,7 @@ class AppProfile {
   const AppProfile(this.name, this.email, this.imageUrl);
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is AppProfile &&
-          runtimeType == other.runtimeType &&
-          name == other.name;
+  bool operator ==(Object other) => identical(this, other) || other is AppProfile && runtimeType == other.runtimeType && name == other.name;
 
   @override
   int get hashCode => name.hashCode;
